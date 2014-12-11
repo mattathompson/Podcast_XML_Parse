7feb0240ddf24629102865b629fc4fd31827186a
Matthew Thompson <matt@mattathompson.org>
2014-12-11 00:20:44 -0500

    Finial commit for podcast app

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -65,6 +65,12 @@ class ItemsController < ApplicationController
 
 
   def destroy
+    @item = Item.find params[:id]
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
 
   end
 
